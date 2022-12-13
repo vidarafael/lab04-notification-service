49911485dac34a666e33d49ebb4f09b97e7ca61c
Rafael Freitas <vidarafael@hotmail.com>
2022-12-12 22:23:43 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { PrismaService } from './prisma.service';
+import { Controller, Get } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
